96bfbb0f5999b447cecf869a5ff396f9ab62c0e6
Aniruddha Shastri <aniruddha.shastri@ni.com>
2018-09-21 14:45:24 -0500

    Move main logic to method

--- CapitalizeFile.py
+++ CapitalizeFile.py
@@ -1,17 +1,17 @@
 import sys
 
+def writeToFileInAllCaps(fileNameToRead, fileNameToWrite):
+    print ('Capitalizing', fileNameToRead)
+    with open(fileNameToRead, 'r') as fileToRead:
+        with open(fileNameToWrite, 'w') as fileToWrite:
+            fileToWrite.write(fileToRead.read().upper())
+            print ('Output written to', fileNameToWrite)
+
 def main():
     if (len(sys.argv) != 3):
         print ('Must provide an input and output file name!')
         return
-    inputFileName = sys.argv[1]
-    outputFileName = sys.argv[2]
-    print ('Capitalizing', inputFileName)
-
-    with open(inputFileName, 'r') as fileToRead:
-        with open(outputFileName, 'w') as fileToWrite:
-            fileToWrite.write(fileToRead.read().upper())            
-            print ('Output written to', outputFileName)
+    writeToFileInAllCaps(sys.argv[1], sys.argv[2])
 
 if __name__ == "__main__": 
     main()
