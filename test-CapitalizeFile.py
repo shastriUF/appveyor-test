cbd5dbc9e5a6a485e2cbf265b341ebaa404d652c
Aniruddha Shastri <aniruddha.shastri@ni.com>
2018-09-21 16:27:29 -0500

    Add unit test for capitalization

--- test-CapitalizeFile.py
+++ test-CapitalizeFile.py
@@ -1,8 +1,19 @@
+import CapitalizeFile
+import os
 import unittest
 
 class TestCapitalization(unittest.TestCase):
     def testUnitTestAssert(self):
         self.assertEqual(1, 1)
+    
+    def testCapitalizeFile(self):
+        with open('.inputFile', 'w') as inputFile:
+            inputFile.write('Hello world!')
+        CapitalizeFile.writeToFileInAllCaps('.inputFile', '.outputFile')
+        with open('.outputFile', 'r') as outputFile:
+            self.assertEqual(outputFile.read(), 'HELLO WORLD!')
+        os.remove('.inputFile')
+        os.remove('.outputFile')
 
 if __name__ == '__main__':
     unittest.main()
