813334d661194be1ad7bc42a9c9f7a1d52f7d7d2
Aniruddha Shastri <aniruddha.shastri@ni.com>
2018-09-21 16:15:27 -0500

    fix spaces and break test again

--- CapitalizeFile.py
+++ CapitalizeFile.py
@@ -1,15 +1,15 @@
 import sys
 
 def writeToFileInAllCaps(fileNameToRead, fileNameToWrite):
-    print ('Capitalizing', fileNameToRead)
+    print('Capitalizing', fileNameToRead)
     with open(fileNameToRead, 'r') as fileToRead:
         with open(fileNameToWrite, 'w') as fileToWrite:
             fileToWrite.write(fileToRead.read().upper())
-            print ('Output written to', fileNameToWrite)
+            print('Output written to', fileNameToWrite)
 
 def main():
     if (len(sys.argv) != 3):
-        print ('Must provide an input and output file name!')
+        print('Must provide an input and output file name!')
         return
     writeToFileInAllCaps(sys.argv[1], sys.argv[2])
 
--- test-CapitalizeFile.py
+++ test-CapitalizeFile.py
@@ -2,7 +2,7 @@ import unittest
 
 class TestCapitalization(unittest.TestCase):
     def ValidateTestAssert(self):
-        self.assertEqual(1, 1)
+        self.assertEqual(1, 2)
 
 if __name__ == '__main__':
     unittest.main()
