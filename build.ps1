be42fdf043ccfb565e4026775c81236ed2075beb
Aniruddha Shastri <aniruddha.shastri@ni.com>
2018-09-26 11:43:47 -0500

    Add NIPM installer

--- build.ps1
+++ build.ps1
@@ -1,16 +1,19 @@
 Write-Output "Installing NIPM"
+$rootDirectory = (Get-Location).Path
+Write-Output "Current directory $rootDirectory"
 
 $nipmDownloadPath = 'http://download.ni.com/support/softlib/AST/NIPM/NIPackageManager18.0.2.exe'
-$nipmInstaller = '.\\install-nipm.exe'
+$nipmInstaller = Join-Path -Path $rootDirectory -ChildPath 'install-nipm.exe'
 Invoke-WebRequest -Uri $nipmDownloadPath -OutFile $nipmInstaller -Verbose
 
 if (![System.IO.File]::Exists($nipmInstaller))
 {
-    Write-Output "Could not find downloaded NIPM installer"
-    return -1
+    throw "Could not find downloaded NIPM installer"
 }
 
-$output = & $nipmInstaller
-Write-Output $output
+Start-Process -FilePath $nipmInstaller -ArgumentList "/Q" -Wait
+Write-Output "NIPM Installed (maybe)"
+
+Remove-Item $nipmInstaller
 
 return
