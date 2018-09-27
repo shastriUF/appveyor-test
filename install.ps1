23f94763e0ef22904fb43bc22be720fa567cdec0
Aniruddha Shastri <aniruddha.shastri@ni.com>
2018-09-27 14:03:52 -0500

    Add sanity checks for files to not exist before they are installed.

--- install.ps1
+++ install.ps1
@@ -10,6 +10,18 @@ function Assert-FileExists {
     }
 }
 
+function Assert-FileDoesNotExist {
+    Param ([string]$path)
+    if ([System.IO.File]::Exists($path))
+    {
+        throw "Found unexpected file at $path."
+    }
+    else 
+    {
+        Write-Output "$path not installed."
+    }
+}
+
 $rootDirectory = (Get-Location).Path
 Write-Output "Current directory $rootDirectory"
 
@@ -18,12 +30,14 @@ if ($install_NIPM)
 {
     $nipmDownloadPath = 'http://download.ni.com/support/softlib/AST/NIPM/NIPackageManager18.5.exe'
     $nipmInstaller = Join-Path -Path $rootDirectory -ChildPath 'install-nipm.exe'
+    Assert-FileDoesNotExist($nipmInstaller)
     Write-Output "Downloading NIPM from $nipmDownloadPath..."
     $webClient = New-Object System.Net.WebClient
     $webClient.DownloadFile($nipmDownloadPath, $nipmInstaller)
     Write-Output "...done"
     Assert-FileExists($nipmInstaller)
     
+    Assert-FileDoesNotExist($nipm)
     Write-Output "Installing NIPM..."
     Start-Process -FilePath $nipmInstaller -ArgumentList "/Q" -Wait
     Write-Output "...done"
@@ -37,6 +51,7 @@ Assert-FileExists($nipm)
 $install_nxg = $true
 if ($install_nxg)
 {
+    Assert-FileDoesNotExist($nxg)
     Write-Output "Installing LabVIEW NXG..."
     # Start-Process -FilePath $nipm -ArgumentList 'install ni-labview-nxg-2.0.0 --temp-feeds="http://download.ni.com/support/nipkg/products/ni-labview-nxg-2.0.0/2.1/released,http://download.ni.com/support/nipkg/products/ni-labview-nxg-2.0.0-rte/2.1/released" --progress-only --accept-eulas --prevent-reboot'
     Start-Process -FilePath $nipm -ArgumentList 'feed-add http://download.ni.com/support/nipkg/products/ni-labview-nxg-2.0.0/2.1/released' -Wait
