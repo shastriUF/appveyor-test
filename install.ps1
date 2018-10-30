d0884bfd225ce91e9de39e21be8c980a2a5aabb0
Aniruddha Shastri <aniruddha.shastri@ni.com>
2018-10-30 10:52:47 -0500

    Refactor Install.ps1 to use common redirection code.

--- install.ps1
+++ install.ps1
@@ -22,9 +22,46 @@ function Assert-FileDoesNotExist {
     }
 }
 
-function WaitForNIPM {
-    $id = (Get-Process 'nipkg').Id
-    Wait-Process -Id $id
+function Run {
+	Param ([string]$fileName, [string]$arguments)
+	
+	$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+	$pinfo.FileName = $fileName
+	$pinfo.RedirectStandardError = $true
+	$pinfo.RedirectStandardOutput = $true
+	$pinfo.UseShellExecute = $false
+	$pinfo.Arguments = $arguments
+	$p = New-Object System.Diagnostics.Process
+	$p.StartInfo = $pinfo
+	
+	$out = New-Object System.Collections.ArrayList
+	$handler = 
+	{
+        if (! [String]::IsNullOrEmpty($EventArgs.Data)) 
+		{
+            $Event.MessageData.Add($EventArgs.Data)
+        }
+    }
+	
+	$outEvent = Register-ObjectEvent -InputObject $p -Action $handler -EventName 'OutputDataReceived' -MessageData $out
+		
+	$p.Start() | Out-Null
+	$p.BeginOutputReadLine()	
+	while (!$p.HasExited)
+	{
+		Wait-Event -Timeout 1
+		while($out.Length -gt 0)
+		{
+			$out[0].ToString()
+			$out.RemoveAt(0)
+		}
+	}
+	while($out.Length -gt 0)
+	{
+		$out[0].ToString()
+		$out.RemoveAt(0)
+	}
+	Unregister-Event -SourceIdentifier $outEvent.Name
 }
 
 $rootDirectory = (Get-Location).Path
@@ -39,7 +76,8 @@ if ($install_NIPM)
     $nipmDownloadPath = 'http://download.ni.com/support/softlib/AST/NIPM/NIPackageManager18.5.1.exe'
     $nipmInstaller = Join-Path -Path $rootDirectory -ChildPath 'install-nipm.exe'
     Assert-FileDoesNotExist($nipm)
-    Write-Output "Downloading NIPM from $nipmDownloadPath..."
+    $time = (Get-Date).ToUniversalTime()
+	Write-Output "Downloading NIPM from $nipmDownloadPath... UTC $time"
     $webClient = New-Object System.Net.WebClient
     $webClient.DownloadFile($nipmDownloadPath, $nipmInstaller)
     $time = (Get-Date).ToUniversalTime()
@@ -61,104 +99,27 @@ if ($install_nxg)
 {
     $nxg = 'C:\Program Files\National Instruments\LabVIEW NXG 2.0\LabVIEW NXG.exe'
     Assert-FileDoesNotExist($nxg)
-    # Start-Process -FilePath $nipm -ArgumentList 'install ni-labview-nxg-2.0.0 --temp-feeds="http://download.ni.com/support/nipkg/products/ni-labview-nxg-2.0.0/2.1/released,http://download.ni.com/support/nipkg/products/ni-labview-nxg-2.0.0-rte/2.1/released" --progress-only --accept-eulas --prevent-reboot'
-    Start-Process -FilePath $nipm -ArgumentList 'feed-add http://download.ni.com/support/nipkg/products/ni-labview-nxg-2.0.0/2.1/released'
-    WaitForNIPM
-    Start-Process -FilePath $nipm -ArgumentList 'feed-add http://download.ni.com/support/nipkg/products/ni-labview-nxg-2.0.0-rte/2.1/released'
-    WaitForNIPM
-    Start-Process -FilePath $nipm -ArgumentList 'feed-add http://download.ni.com/support/nipkg/products/ni-labview-nxg-2.0.0-web-module/2.1/released'
-    WaitForNIPM
-    Start-Process -FilePath $nipm -ArgumentList 'update'
-    WaitForNIPM
-    Write-Output "Installing NI Certificates..."
-    # Start-Process -FilePath $nipm -ArgumentList 'install ni-certificates --accept-eulas --assume-yes --verbose'
-	$pinfo = New-Object System.Diagnostics.ProcessStartInfo
-	$pinfo.FileName = $nipm
-	$pinfo.RedirectStandardError = $true
-	$pinfo.RedirectStandardOutput = $true
-	$pinfo.UseShellExecute = $false
-	$pinfo.Arguments = "install ni-certificates --accept-eulas --assume-yes --verbose"
-	$p = New-Object System.Diagnostics.Process
-	$p.StartInfo = $pinfo
 	
-	$SOut = New-Object System.Collections.ArrayList
-	$handler = 
-	{
-        if (! [String]::IsNullOrEmpty($EventArgs.Data)) 
-		{
-            $Event.MessageData.Add($EventArgs.Data)
-        }
-    }
+	Write-Output "Adding LabVIEW NXG feeds to NI Package Manager"
+    Run $nipm 'feed-add http://download.ni.com/support/nipkg/products/ni-labview-nxg-2.0.0/2.1/released'
+	Run $nipm 'feed-add http://download.ni.com/support/nipkg/products/ni-labview-nxg-2.0.0-rte/2.1/released'
+    Run $nipm 'feed-add http://download.ni.com/support/nipkg/products/ni-labview-nxg-2.0.0-web-module/2.1/released'
+    Run $nipm 'update'
 	
-	$SOutEvent = Register-ObjectEvent -InputObject $p -Action $handler -EventName 'OutputDataReceived' -MessageData $SOut
-		
-	$p.Start() | Out-Null
-	$p.BeginOutputReadLine()	
-	while (!$p.HasExited)
-	{
-		Wait-Event -Timeout 1
-		while($SOut.Length -gt 0)
-		{
-			$SOut[0].ToString()
-			$SOut.RemoveAt(0)
-		}
-	}
-	while($SOut.Length -gt 0)
-	{
-		$SOut[0].ToString()
-		$SOut.RemoveAt(0)
-	}
-	Unregister-Event -SourceIdentifier $SOutEvent.Name
-    # WaitForNIPM
+	Write-Output "Installing NI Certificates..."
+	Run $nipm 'install ni-certificates --accept-eulas --assume-yes --verbose'
+	$time = (Get-Date).ToUniversalTime()
+    Write-Output "...done at UTC $time"
+    
+	Write-Output "Installing LabVIEW NXG..."
+	Run $nipm 'install ni-labview-nxg-2.0.0 --accept-eulas --assume-yes --verbose'
     $time = (Get-Date).ToUniversalTime()
     Write-Output "...done at UTC $time"
-    Write-Output "Installing LabVIEW NXG..."
-	# Start-Process -FilePath $nipm -ArgumentList 'install ni-labview-nxg-2.0.0 --accept-eulas --assume-yes --verbose'
-	$pinfo = New-Object System.Diagnostics.ProcessStartInfo
-	$pinfo.FileName = $nipm
-	$pinfo.RedirectStandardError = $true
-	$pinfo.RedirectStandardOutput = $true
-	$pinfo.UseShellExecute = $false
-	$pinfo.Arguments = "install ni-labview-nxg-2.0.0 --accept-eulas --assume-yes --verbose"
-	$p = New-Object System.Diagnostics.Process
-	$p.StartInfo = $pinfo
-	
-	$SOut = New-Object System.Collections.ArrayList
-	$handler = 
-	{
-        if (! [String]::IsNullOrEmpty($EventArgs.Data)) 
-		{
-            $Event.MessageData.Add($EventArgs.Data)
-        }
-    }
-	
-	$SOutEvent = Register-ObjectEvent -InputObject $p -Action $handler -EventName 'OutputDataReceived' -MessageData $SOut
-		
-	$p.Start() | Out-Null
-	$p.BeginOutputReadLine()	
-	while (!$p.HasExited)
-	{
-		Wait-Event -Timeout 1
-		while($SOut.Length -gt 0)
-		{
-			$SOut[0].ToString()
-			$SOut.RemoveAt(0)
-		}
-	}
-	while($SOut.Length -gt 0)
-	{
-		$SOut[0].ToString()
-		$SOut.RemoveAt(0)
-	}
-	Unregister-Event -SourceIdentifier $SOutEvent.Name
-	# WaitForNIPM
+    
+	Write-Output "Installing LabVIEW NXG Web Module..."
+    Run $nipm 'install ni-labview-nxg-2.0.0-web-module --accept-eulas --assume-yes --verbose'
     $time = (Get-Date).ToUniversalTime()
     Write-Output "...done at UTC $time"
-    # Write-Output "Installing LabVIEW NXG Web Module..."
-    # Start-Process -FilePath $nipm -ArgumentList 'install ni-labview-nxg-2.0.0-web-module --accept-eulas --assume-yes --verbose'
-    # WaitForNIPM
-    # $time = (Get-Date).ToUniversalTime()
-    # Write-Output "...done at UTC $time"
     Assert-FileExists($nxg)
 }
 
